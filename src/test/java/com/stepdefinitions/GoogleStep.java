31740823e955d1e410056842f7df215268248995
Mukeshgiri <mukeshgiri020@gmail.com>
2022-08-31 00:25:30 +0530

    Initial commit

---
+++ src/test/java/com/stepdefinitions/GoogleStep.java
@@ -0,0 +1,25 @@
+package com.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class GoogleStep {
+
+    @Given("Open Browser")
+    public void open_browser() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Enter url")
+    public void enter_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Check user on Google")
+    public void check_user_on_google() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ src/test/resources/features/Google.feature
@@ -0,0 +1,6 @@
+Feature: Google Testing
+
+  Scenario: Successfully open google
+    Given Open Browser
+    Then Enter url
+    Then Check user on Google
